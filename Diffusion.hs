1657148cbd1daef9d2557dbaed615678d56350ab
Octavian Geagla <ogeagla@gmail.com>
2014-07-08 08:33:10 -0600

    Update to iterations, now use proper diffusion equations.  Current run shows a transition to a linear density function, as would be expected between constant source and perfect sink boundary conditions.

--- Diffusion.hs
+++ Diffusion.hs
@@ -29,17 +29,17 @@ data DiffusionProcess = LeftBoundary | Middle | RightBoundary
 diffusion_steps :: [DiffusionProcess]
 diffusion_steps = [LeftBoundary] Prelude.++ Prelude.replicate size Middle Prelude.++ [RightBoundary]
 
-type DiffusionRuntime = ([DiffusionProcess], Vector Double, Vector Double, Int) 
+type DiffusionRuntime = ([DiffusionProcess], Vector Double, Vector Double, Double, Double) 
 
 runDiffusionProcess :: DiffusionRuntime -> DiffusionRuntime
-runDiffusionProcess (LeftBoundary:dps, old_cells, new_cells, s) = runDiffusionProcess (dps, old_cells,                    new_cells Data.Vector.++ diffuse (LeftBoundary,  Data.Vector.take 2 old_cells,s), s)
-runDiffusionProcess (Middle:dps,       old_cells, new_cells, s) = runDiffusionProcess (dps, Data.Vector.drop 1 old_cells, new_cells Data.Vector.++ diffuse (Middle,        Data.Vector.take 3 old_cells,s), s)
-runDiffusionProcess ([RightBoundary],  old_cells, new_cells, s) =                     ([],  old_cells,                    new_cells Data.Vector.++ diffuse (RightBoundary, Data.Vector.take 2 old_cells,s), s)
+runDiffusionProcess (LeftBoundary:dps, old_cells, new_cells, dt, dx) = runDiffusionProcess (dps, old_cells,                    new_cells Data.Vector.++ diffuse(LeftBoundary,  Data.Vector.take  2 old_cells, dt, dx), dt, dx)
+runDiffusionProcess (Middle:dps,       old_cells, new_cells, dt, dx) = runDiffusionProcess (dps, Data.Vector.drop 1 old_cells, new_cells Data.Vector.++ diffuse(Middle,        Data.Vector.take 3 old_cells, dt, dx), dt, dx)
+runDiffusionProcess ([RightBoundary],  old_cells, new_cells, dt, dx) =                     ([],  old_cells,                    new_cells Data.Vector.++ diffuse(RightBoundary, Data.Vector.take 2 old_cells, dt, dx), dt, dx)
 
-diffuse :: (DiffusionProcess, Vector Double, Int) -> Vector Double
-diffuse (LeftBoundary,  cells, s)  = fromList[(Data.Vector.foldl (+) 0.0 cells) / 3.0]::Vector Double
-diffuse (Middle,        cells, s)  = fromList[(Data.Vector.foldl (+) 0.0 cells) / 3.0]::Vector Double
-diffuse (RightBoundary, cells, s)  = fromList[(Data.Vector.foldl (+) 0.0 cells) / 3.0]::Vector Double
+diffuse :: (DiffusionProcess, Vector Double, Double, Double) -> Vector Double
+diffuse (LeftBoundary,  cells, dt, dx)  = fromList ([(cells ! 0) + (dt/(dx*dx))*((cells ! 1) - 2.0*(cells ! 0) + (5.0))])                                          
+diffuse (Middle,        cells, dt, dx)  = fromList ([(cells ! 1) + (dt/(dx*dx))*((cells ! 2) - 2.0*(cells ! 1) + (cells ! 0))])
+diffuse (RightBoundary, cells, dt, dx)  = fromList ([(cells ! 1) + (dt/(dx*dx))*((0.0) - 2.0*(cells ! 1) + (cells ! 0))])
 
 sumSqr :: Vector Double -> Double
 sumSqr v = Data.Vector.sum $ Data.Vector.zipWith (*) v v
@@ -51,17 +51,20 @@ isConverged v1 v2 thresh =  sqrt (sumSqr (Data.Vector.zipWith (-) v2 v1)) < thre
   --sqrt (Data.Vector.sum $ Data.Vector.zipWith (*) v1  v2)  > thresh
 
 initialRuntime :: DiffusionRuntime
-initialRuntime = (diffusion_steps, fromList [1.0,2.0,3.0,4.0], fromList [], 4)
+initialRuntime = (diffusion_steps, fromList [1.0,2.0,3.0,4.0], fromList [], 0.1, 1.0)
 
-runUntilConverged :: DiffusionRuntime -> DiffusionRuntime -> Double -> Int-> (Int, Vector Double) 
-runUntilConverged (dps1, old1, new1, size1) (dps2, old2, new2, size2) thresh iter
+iterateDiffusion :: DiffusionRuntime -> DiffusionRuntime -> Double -> Int-> (Int, Vector Double) 
+iterateDiffusion (dps1, old1, new1, dt1, dx1) (dps2, old2, new2, dt2, dx2) thresh iter
   | isConverged old1 new2 thresh  = (iter, new2)
-  | otherwise = runUntilConverged (dps1, new2, fromList [], size2) (runDiffusionProcess (dps1, new2, fromList [], size2)) thresh (iter + 1)
+  | otherwise = iterateDiffusion (dps1, new2, fromList [], dt1, dx1) (runDiffusionProcess (dps1, new2, fromList [], dt1, dx1)) thresh (iter + 1)
 
+
+convergence_threshold :: Double
+convergence_threshold = 0.0005
 
 runDiffusionUntilConvergence :: IO ()
-runDiffusionUntilConvergence = do print initialRuntime
-                                  print (runUntilConverged initialRuntime (runDiffusionProcess initialRuntime) 0.001 0)
+runDiffusionUntilConvergence = do print ("init runtime: ", initialRuntime)
+                                  print (iterateDiffusion initialRuntime (runDiffusionProcess initialRuntime) convergence_threshold 1)
 
 instance Show DiffusionProcess where
   show (LeftBoundary) = "LeftBoundary"
